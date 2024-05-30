8d10a549d213258d833797e1c34121fd8c4a19bf
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-05-30 19:55:20 +0200

    nice

---
+++ README.md
@@ -0,0 +1,6 @@
+# lcs_rs
+
+
+Longest common subsequence implementation in rust.
+
+https://en.wikipedia.org/wiki/Longest_common_subsequence
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,37 @@
-use std::cmp::max;
+use std::{cmp::max, fmt::Debug};
 
 pub fn lcs(s1: &str, s2: &str) -> usize {
 
     let mut mat: Vec<Vec<i32>> = vec![vec![0; s1.len() + 1]; s2.len() + 1];
 
-    for (i, u) in s1.char_indices() {
-        for (j, v) in s2.char_indices() {
+    let mut s1_iter = s1.chars();
+    let mut u = None;
+
+    for i in 0..=s1.len() {
+        let mut s2_iter = s2.chars();
+        let mut v = None;
+
+        for j in 0..=s2.len() {
             if i == 0 || j == 0 {
                 mat[i][j] = 0;
-            } else if u == v {
+            } else if u.unwrap() == v.unwrap() {
                 mat[i][j] = mat[i - 1][j - 1] + 1;
             } else {
                 mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]);
             }
+            v = s2_iter.next();
         }
+        u = s1_iter.next();
     }
+
 
     let mut s1_iter = s1.char_indices().rev().peekable();
     let mut s2_iter = s1.char_indices().rev().peekable();
 
     let mut res = 0;
 
-    for v in &mat {
-        println!("{:?}", v);
-    }
+    print_mat(&mat);
+    
 
     while let (Some((i, u)), Some((j, v))) = (s1_iter.peek(), s2_iter.peek()) {
 
@@ -42,6 +50,17 @@ pub fn lcs(s1: &str, s2: &str) -> usize {
     res
 }
 
+
+fn print_mat<T: Debug>(mat: &Vec<Vec<T>>) {
+    for j in 0..mat[0].len() {
+        for i in 0..mat.len() {
+            print!("{:?} ", mat[i][j])
+        }
+        println!()
+    }
+
+    println!();
+}
 
 #[cfg(test)]
 mod test {
